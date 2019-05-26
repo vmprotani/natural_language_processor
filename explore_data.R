94aaa17e33f5c3b6c9d7028df442fddc491208ca
Vince Protani <vincent.protani@gmail.com>
2019-05-26 13:48:13 -0400

    update separated code in Rmd

--- explore_data.R
+++
@@ -1,7 +0,0 @@
-library(NLP)
-
-dataDir <- "./tidy_data/"
-dataFiles <- paste(dataDir, c("en_US.blogs.lines.txt", "en_US.news.lines.txt", "en_US.twitter.lines.txt"), 
-                   sep="")
-
-tidyData <- lapply(dataFiles, read.table, header=TRUE, sep=",")
