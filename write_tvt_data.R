b79cda7ff419bc1f8912db4a3ed5efe76b2c75b7
Vince Protani <vincent.protani@gmail.com>
2019-05-28 21:45:02 -0400

    clean up tvt data write

--- write_tvt_data.R
+++ write_tvt_data.R
@@ -13,20 +13,24 @@ dir <- "./tidy_data/"
 tvt.files <- paste(dir,"en_US.",sources,".tvt.txt",sep="")
 output.dirs <- c("./train/","./validate/","./test/")
 
+# read data files
 data <- lapply(tvt.files, read.table, header=TRUE, sep="\t", stringsAsFactors=FALSE, quote="") %>% lapply(as_tibble)
 
+# declare filtering functions
 is_word <- function(x) { grepl("^[a-z\']+$", x) }
 vowel_repeats <- function(x) { grepl("(a|i|u){2,}|(e|o){3,}", x) }
 
+# create ngrams and separate by word
 create_ngrams <- function(n) {
-  ngrams <- lapply(data, unnest_tokens, ngram, sentence, token="ngrams", n=2)
+  ngrams <- lapply(data, unnest_tokens, ngram, sentence, token="ngrams", n=n)
   ngrams <- lapply(ngrams, function(x) x[!is.na(x$ngram), ])
-  numWords <- 1:n
-  ngramSep <- bind_rows(ngrams) %>% separate("ngram", paste("word", numWords, sep=""), sep=" ")
-  ngramSep %>% filter(is_word(word1) & is_word(word2)) %>% 
-    filter(!(vowel_repeats(word1) | vowel_repeats(word2))) %>% filter(!(word1==word2))
+
+  num.words <- 1:n
+  words <- paste("word", num.words, sep="")
+  ngramSep <- bind_rows(ngrams) %>% separate("ngram", words, sep=" ")
 }
 
+# write ngrams to file
 write_ngrams <- function(ngrams, to.append) {
   in.train <- createDataPartition(1:NROW(ngrams), p=0.6, list=FALSE)
   ngrams.train <- ngrams[in.train,]
@@ -46,14 +50,27 @@ write_ngrams <- function(ngrams, to.append) {
               row.names=FALSE, col.names=FALSE, quote=FALSE, append=to.append)
 }
 
+# create and write bigrams
 bigrams <- create_ngrams(2)
+bigrams <- bigrams %>% filter(is_word(word1) & is_word(word2)) %>% 
+  filter(!(vowel_repeats(word1) | vowel_repeats(word2))) %>% 
+  filter(!(word1==word2))
 write_ngrams(bigrams, FALSE)
 rm(bigrams)
 
+# create and write trigrams
 trigrams <- create_ngrams(3)
+trigrams <- trigrams %>% filter(is_word(word1) & is_word(word2) & is_word(word3)) %>% 
+  filter(!(vowel_repeats(word1) | vowel_repeats(word2) | vowel_repeats(word3))) %>% 
+  filter(!(word1==word2 | word2==word3))
 write_ngrams(trigrams, TRUE)
 rm(trigrams)
 
+#create and write quadrigrams
 quadrigrams <- create_ngrams(4)
+quadrigrams <- quadrigrams %>% 
+  filter(is_word(word1) & is_word(word2) & is_word(word3) & is_word(word4)) %>% 
+  filter(!(vowel_repeats(word1) | vowel_repeats(word2) | vowel_repeats(word3) | vowel_repeats(word4))) %>% 
+  filter(!(word1==word2 | word2==word3 | word3==word4))
 write_ngrams(quadrigrams, TRUE)
 rm(quadrigrams)
