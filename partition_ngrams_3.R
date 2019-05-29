e6ca84a03839fe33b77fece8811876812c263fe9
Vince Protani <vincent.protani@gmail.com>
2019-05-28 21:52:45 -0400

    remove total word count from partitioned ngrams

--- partition_ngrams_3.R
+++ partition_ngrams_3.R
@@ -3,9 +3,6 @@ sort3 <- function(x) {
     filter(!(vowelRepeats(word1) | vowelRepeats(word2) | vowelRepeats(word3))) %>% 
     filter(!(word1==word2 | word2==word3)) %>%  group_by(word1, word2) %>% count(word3) %>% 
     arrange(word1, word2, desc(n))
-  totals3 <- set3 %>% ungroup() %>% select(word3) %>% count(word3)
-  inner_join(set3, totals3, by="word3") %>% arrange(word1, word2, desc(n.x), desc(n.y), word3) %>%
-    ddply(.(word1, word2), function(x) x[1:min(3, NROW(x)),])
 }
 filtered3 <- sort3(ngram3$count[rbinom(NROW(ngram3$count), 1, prob=0.6)==1,])
 rm(ngram3)
--- partition_ngrams_4.R
+++ partition_ngrams_4.R
@@ -3,9 +3,6 @@ sort4 <- function(x) {
     filter(!(vowelRepeats(word1) | vowelRepeats(word2) | vowelRepeats(word3) | vowelRepeats(word4))) %>% 
     filter(!(word1==word2 | word2==word3 | word3==word4)) %>% group_by(word1, word2, word3) %>% 
     count(word4) %>% arrange(word1, word2, word3, desc(n))
-  totals4 <- set4 %>% ungroup() %>% select(word4) %>% count(word4)
-  inner_join(set4, totals4, by="word4") %>% arrange(word1, word2, word3, desc(n.x), desc(n.y), word4) %>%
-    ddply(.(word1, word2, word3), function(x) x[1:min(3, NROW(x)),])
 }
 filtered4 <- sort4(ngram4$count[rbinom(NROW(ngram4$count), 1, prob=0.6)==1,])
 rm(ngram4)
