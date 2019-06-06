688a9fac34a33548b02ed007acdb35e9c6fedd6d
Vince Protani <vincent.protani@gmail.com>
2019-06-05 21:11:34 -0400

    finalize test function

--- test/test_katz.R
+++ test/test_katz.R
@@ -1,18 +1,28 @@
 library(data.table)
-source("new_katz_backoff.R")
+source("../src/katz_backoff.R")
 
-test <- fread("../model_data/test.txt")
-names(test) <- c("input", "result")
-test <- test[grepl(" ", input),]
-num.tests <- 1:NROW(test)
+test <- fread("../test/tests.txt", sep=",")
+index <- 1:NROW(test)
 
 # assume all tests fail
-test$success <- 0
+test$success <- FALSE
 
-for (II in test) {
-  res <- run_backoff(test$input[II])
-  if (test$result[II] %in% res$prediction) { test$success[II] <- 1 }
+# initialize time for each test
+test$time <- 0
+
+run_tests <- function(num.out=1) {
+  for (II in index) {
+    # run the prediction algorithm
+    start.time <- Sys.time()
+    res <- run_backoff(test[II]$input, num.out)
+    end.time <- Sys.time()
+    
+    # mark success
+    if (test[II,]$result %in% res$prediction) { test[II,]$success <- TRUE }
+    
+    # record time
+    test[II,]$time <- end.time - start.time
+  }
+  data.table(num.tests=NROW(test), predictions=num.out, accuracy=sum(test$success)/NROW(test), 
+                     avg.time=mean(test$time))
 }
-disp <- data.table(no.tests=NROW(tests), no.success=sum(tests$success), 
-                   percent=passed)
-disp
